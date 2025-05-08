41fa6e8d0002f45356a0d7810388b7846f26846d
Greeshma22000 <madalagreeshma@gmail.com>
2025-05-08 16:14:20 +0530

    Navigation bar

---
+++ NavigationBar/script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const side = document.querySelector('.sidebar')
+    side.style.display = 'flex'
+}
+
+function closeSidebar(){
+    const side = document.querySelector('.sidebar')
+    side.style.display = "none"
+}
